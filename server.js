c16ca28e8a95992f8fbdbd868b2fb47f61702102
javrong <sunumd@yahoo.com>
2025-04-16 13:27:27 +0700

    set header nocache module

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.use(helmet.noSniff());
 app.use(helmet.xssFilter({}));
 //app.use(helmet.noCache());
 
-app.use(nocache());
+app.use(nocache(function (req, res, next) {
+  res.setHeader('Surrogate-Control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Expires', '0');
+}));
 
 // Enabling header PHP-7.4.3
 app.use(function (req, res, next) {
